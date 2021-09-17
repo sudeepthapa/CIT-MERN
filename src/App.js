e7310e63316c535b1f4b09e5c46603182a25f8bf
Sudip Thapa <sthapa@deerhold.org>
2021-09-17 17:26:15 +0545

    stateful component and lifecycles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, Fragment} from 'react';
+import './App.css'
+class App extends Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  state = { // useState
+    count: 0,
+    shouldHide: false
+  }
+
+  // lifecycles
+
+  componentDidMount = () => {   // useEffect
+    console.log('Component did mount')
+  }
+
+  componentWillUnmount = () => { // useEffect
+    console.log('component will unmount')
+  }
+
+  shouldComponentUpdate = (nextProp, nextState) => { // useEffect
+    return nextState.count !== 0;
+  }
+
+
+  // lifecyles end
+
+
+  increment = () => {
+    this.setState({
+      ...this.state,
+      count: this.state.count + 1,
+    })
+  }
+
+  decrement = () => {
+    this.setState({
+      ...this.state,
+      count: this.state.count - 1
+    })
+  }
+
+  hide = () => {
+    this.setState({
+      ...this.state,
+      shouldHide: !this.state.shouldHide
+    })
+  }
+
+  render(){
+    console.log('render')
+    return <div className="container">
+        { !this.state.shouldHide && <h1>{this.state.count}</h1>}
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        <button onClick={this.hide}>{ !this.state.shouldHide ? 'Hide' : 'Show'}</button>
     </div>
-  );
+  }
+
 }
 
 export default App;
