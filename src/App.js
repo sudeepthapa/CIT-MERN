615bb53bcb7f0088c664a4a4f53861ea4eb77202
Sudip Thapa <sthapa@deerhold.org>
2021-09-21 16:26:03 +0545

    functional state approach

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component, Fragment } from 'react';
 import './App.css'
 class App extends Component{
 
@@ -58,4 +58,33 @@ class App extends Component{
 
 }
 
-export default App;
+
+const FunctionalApp = () => {
+
+  const [count, setCount] = React.useState(0);
+  const [shouldHide, setShouldHide] = React.useState(false);
+  const [person, setPerson] = React.useState({
+    name: 'alex',
+    age: 12
+  })
+
+  const increment = () => setCount(count + 1);
+  
+  const decrement = () => setCount(count - 1);
+
+  const hide = () => setShouldHide(!shouldHide);
+
+  const changePerson = () => setPerson({
+    ...person,
+    name: 'blex'
+  });
+
+  return <div className="container">
+        { !shouldHide && <h1>{count}</h1>}
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={hide}>{ !shouldHide ? 'Hide' : 'Show'}</button>
+    </div>
+}
+
+export default FunctionalApp;
